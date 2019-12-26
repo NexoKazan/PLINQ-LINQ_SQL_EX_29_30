3047f1100070f94b323d9f9134ed424641b0b5ae
IAKazantsev <IAKazantsev@DIT-07-230-02>
2019-12-26 13:47:50 +0300

    Добавьте файлы проекта.

---
+++ LINQ/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace LINQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_Q29 = new System.Windows.Forms.Button();
+            this.btn_Q30 = new System.Windows.Forms.Button();
+            this.textBox_time = new System.Windows.Forms.TextBox();
+            this.checkBox_Plinq = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(775, 358);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_Q29
+            // 
+            this.btn_Q29.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_Q29.Location = new System.Drawing.Point(13, 378);
+            this.btn_Q29.Name = "btn_Q29";
+            this.btn_Q29.Size = new System.Drawing.Size(75, 23);
+            this.btn_Q29.TabIndex = 1;
+            this.btn_Q29.Text = "Q29";
+            this.btn_Q29.UseVisualStyleBackColor = true;
+            this.btn_Q29.Click += new System.EventHandler(this.btn_Q29_Click);
+            // 
+            // btn_Q30
+            // 
+            this.btn_Q30.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_Q30.Location = new System.Drawing.Point(12, 407);
+            this.btn_Q30.Name = "btn_Q30";
+            this.btn_Q30.Size = new System.Drawing.Size(75, 23);
+            this.btn_Q30.TabIndex = 2;
+            this.btn_Q30.Text = "Q30";
+            this.btn_Q30.UseVisualStyleBackColor = true;
+            this.btn_Q30.Click += new System.EventHandler(this.btn_Q30_Click);
+            // 
+            // textBox_time
+            // 
+            this.textBox_time.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox_time.Location = new System.Drawing.Point(95, 380);
+            this.textBox_time.Name = "textBox_time";
+            this.textBox_time.ReadOnly = true;
+            this.textBox_time.Size = new System.Drawing.Size(100, 20);
+            this.textBox_time.TabIndex = 3;
+            // 
+            // checkBox_Plinq
+            // 
+            this.checkBox_Plinq.AutoSize = true;
+            this.checkBox_Plinq.Location = new System.Drawing.Point(95, 412);
+            this.checkBox_Plinq.Name = "checkBox_Plinq";
+            this.checkBox_Plinq.Size = new System.Drawing.Size(58, 17);
+            this.checkBox_Plinq.TabIndex = 4;
+            this.checkBox_Plinq.Text = "PLINQ";
+            this.checkBox_Plinq.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox_Plinq);
+            this.Controls.Add(this.textBox_time);
+            this.Controls.Add(this.btn_Q30);
+            this.Controls.Add(this.btn_Q29);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_Q29;
+        private System.Windows.Forms.Button btn_Q30;
+        private System.Windows.Forms.TextBox textBox_time;
+        private System.Windows.Forms.CheckBox checkBox_Plinq;
+    }
+}
+
---
+++ LINQ/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void Q29()
+        {
+            Console.WriteLine(Environment.NewLine + "Q29" + Environment.NewLine);
+            //SELECT i.point, i.date, i.inc, o.out
+            //FROM income_o i
+            //    LEFT JOIN outcome_o o ON i.point = o.point AND i.date = o.date
+            DataBaseDataContext context = new DataBaseDataContext();
+
+            Console.WriteLine(Environment.NewLine + "INCOME" + Environment.NewLine);
+
+            var income_res = from i1 in context.Income_o
+                from o1 in context.Outcome_o.Where(x => i1.point == x.point && i1.date == x.date).DefaultIfEmpty()
+                select new {i1.point, i1.date, inc = (decimal?)i1.inc, outc = (decimal?)o1.@out};
+            foreach (var t in income_res)
+            {
+                Console.WriteLine(t.point + "\t" + t.date + "\t" + t.inc + "\t" + t.outc);
+            }
+
+            //SELECT o.point, o.date, i.inc, o.out
+            //FROM outcome_o o
+            //    LEFT JOIN income_o i ON o.point = i.point AND o.date = i.date
+
+            Console.WriteLine(Environment.NewLine + "OUTCOME" + Environment.NewLine);
+
+            var outcome_res = from o1 in context.Outcome_o
+                from i1 in context.Income_o.Where(x => o1.point == x.point && o1.date == x.date).DefaultIfEmpty()
+                select new {o1.point, o1.date, inc = (decimal?)i1.inc, outc = (decimal?)o1.@out};
+            foreach (var t in outcome_res)
+            {
+                Console.WriteLine(t.point + "\t" + t.date + "\t" + t.inc + "\t" + t.outc);
+            }
+            
+            Console.WriteLine(Environment.NewLine + "UNION" + Environment.NewLine);
+
+            var union_res = income_res.Union(outcome_res);
+
+            foreach (var t in union_res)
+            {
+                Console.WriteLine(t.point + "\t" + t.date + "\t" + t.inc + "\t" + t.outc);
+            }
+            dataGridView1.DataSource = union_res;
+        }
+        private void Q29_P()
+        {
+            Console.WriteLine(Environment.NewLine + "Q29_P" + Environment.NewLine);
+            DataBaseDataContext context = new DataBaseDataContext();
+
+            var union_V2 = (
+                    from i1 in context.Income_o
+                    from o1 in context.Outcome_o.Where(x => i1.point == x.point && i1.date == x.date).DefaultIfEmpty()
+                    select new {i1.point, i1.date, inc = (decimal?) i1.inc, outc = (decimal?) o1.@out}).AsParallel()
+                .Union((
+                    from o1 in context.Outcome_o
+                    from i1 in context.Income_o.Where(x => o1.point == x.point && o1.date == x.date).DefaultIfEmpty()
+                    select new {o1.point, o1.date, inc = (decimal?) i1.inc, outc = (decimal?) o1.@out}).AsParallel());
+            
+            
+
+            foreach (var t in union_V2)
+            {
+                Console.WriteLine(t.point + "\t" + t.date + "\t" + t.inc + "\t" + t.outc);
+            }
+            var unList = union_V2.ToList();
+            dataGridView1.DataSource = unList;
+        }
+        private void Q30()
+        {
+            Console.WriteLine(Environment.NewLine + "Q30" + Environment.NewLine);
+
+            DataBaseDataContext context = new DataBaseDataContext();
+
+            Console.WriteLine(Environment.NewLine + "INCOME" + Environment.NewLine);
+
+            var res_income = context.Income.GroupBy(i => new { i.point, i.date }).Select(i => new
+            {
+                point = i.Key.point,
+                date = i.Key.date,
+                s_o = (decimal?)0.0m,
+                s_i = (decimal?)i.Sum(s => s.inc)
+            }).OrderBy(o => o.point).ThenBy(o => o.date);
+            foreach (var inco in res_income)
+            {
+                Console.WriteLine(inco.point + "\t" + inco.date + "\t" + inco.s_o + "\t" + inco.s_i);
+            }
+
+            Console.WriteLine(Environment.NewLine + "OUTCOME" + Environment.NewLine);
+
+            var res_outcome = context.Outcome.GroupBy(i => new { i.point, i.date }).Select(i => new
+            {
+                point = i.Key.point,
+                date = i.Key.date,
+                s_o = (decimal?)i.Sum(s => s.@out),
+                s_i = (decimal?)0.0m
+            }).OrderBy(o => o.point).ThenBy(o => o.date);
+            foreach (var outco in res_outcome)
+            {
+                //Console.WriteLine("FORECH");
+
+                Console.WriteLine(outco.point + "\t" + outco.date + "\t" + outco.s_o + "\t" + outco.s_i);
+
+            }
+
+            Console.WriteLine(Environment.NewLine + "UNION" + Environment.NewLine);
+
+            var union = res_income.Union(res_outcome).GroupBy(i => new { i.point, i.date }).Select(i => new
+            {
+                point = i.Key.point,
+                date = i.Key.date,
+                s_o = i.Sum(s => s.s_o),
+                s_i = i.Sum(s => s.s_i),
+            }).OrderBy(o => o.point).ThenBy(o => o.date);
+
+            foreach (var row in union)
+            {
+                //Console.WriteLine("FORECH");
+                Console.WriteLine(row.point + "\t" + row.date + "\t" + row.s_o + "\t" + row.s_i);
+            }
+
+            var union_null = union.Select(s => new
+            {
+                Point = s.point,
+                Date = s.date,
+                Outcome = s.s_o == 0.0m ? null : s.s_o,
+                Income = s.s_i == 0.0m ? null : s.s_i
+            });
+
+            Console.WriteLine(Environment.NewLine + "UNION_NULL" + Environment.NewLine);
+
+            foreach (var row in union_null)
+            {
+                //Console.WriteLine("FORECH");
+                Console.WriteLine(row.Point + "\t" + row.Date + "\t" + row.Outcome + "\t" + row.Income);
+            }
+            dataGridView1.DataSource = union_null;
+        }
+        private void Q30_P()
+        {
+            Console.WriteLine(Environment.NewLine + "Q30_P" + Environment.NewLine);
+
+            DataBaseDataContext context = new DataBaseDataContext();
+
+            Console.WriteLine(Environment.NewLine + "INCOME" + Environment.NewLine);
+
+            var res_income = context.Income.GroupBy(i => new { i.point, i.date }).AsParallel().Select(i => new
+            {
+                point = i.Key.point,
+                date = i.Key.date,
+                s_o = (decimal?)0.0m,
+                s_i = (decimal?)i.Sum(s => s.inc)
+            }).OrderBy(o => o.point).ThenBy(o => o.date);
+            foreach (var inco in res_income)
+            {
+                Console.WriteLine(inco.point + "\t" + inco.date + "\t" + inco.s_o + "\t" + inco.s_i);
+            }
+
+            Console.WriteLine(Environment.NewLine + "OUTCOME" + Environment.NewLine);
+
+            var res_outcome = context.Outcome.GroupBy(i => new { i.point, i.date }).AsParallel().Select(i => new
+            {
+                point = i.Key.point,
+                date = i.Key.date,
+                s_o = (decimal?)i.Sum(s => s.@out),
+                s_i = (decimal?)0.0m
+            }).OrderBy(o => o.point).ThenBy(o => o.date);
+            foreach (var outco in res_outcome)
+            {
+                //Console.WriteLine("FORECH");
+
+                Console.WriteLine(outco.point + "\t" + outco.date + "\t" + outco.s_o + "\t" + outco.s_i);
+
+            }
+
+            Console.WriteLine(Environment.NewLine + "UNION" + Environment.NewLine);
+
+            var union = res_income.Union(res_outcome).GroupBy(i => new { i.point, i.date }).Select(i => new
+            {
+                point = i.Key.point,
+                date = i.Key.date,
+                s_o = i.Sum(s => s.s_o),
+                s_i = i.Sum(s => s.s_i),
+            }).AsParallel().OrderBy(o => o.point).ThenBy(o => o.date);
+
+            foreach (var row in union)
+            {
+                //Console.WriteLine("FORECH");
+                Console.WriteLine(row.point + "\t" + row.date + "\t" + row.s_o + "\t" + row.s_i);
+            }
+
+            var union_null = union.Select(s => new
+            {
+                Point = s.point,
+                Date = s.date,
+                Outcome = s.s_o == 0.0m ? null : s.s_o,
+                Income = s.s_i == 0.0m ? null : s.s_i
+            });
+
+            Console.WriteLine(Environment.NewLine + "UNION_NULL" + Environment.NewLine);
+
+            foreach (var row in union_null)
+            {
+                //Console.WriteLine("FORECH");
+                Console.WriteLine(row.Point + "\t" + row.Date + "\t" + row.Outcome + "\t" + row.Income);
+            }
+
+            var unList = union_null.ToList();
+            dataGridView1.DataSource = unList;
+            dataGridView1.Update();
+            if(
+                dataGridView1.InvokeRequired)
+                MessageBox.Show("INVOKE!!!");
+        }
+        
+        private void btn_Q29_Click(object sender, EventArgs e)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            if(!checkBox_Plinq.Checked)
+                Q29();
+            else
+            {
+                Q29_P();
+            }
+            watch.Stop();
+            textBox_time.Text = watch.ElapsedMilliseconds.ToString();
+        }
+
+        private void btn_Q30_Click(object sender, EventArgs e)
+        { 
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            if(!checkBox_Plinq.Checked)
+                Q30();
+            else
+            {
+                Q30_P();
+            }
+            watch.Stop();
+            textBox_time.Text = watch.ElapsedMilliseconds.ToString();
+        }
+    }
+}
